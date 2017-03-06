000c53c7e019ad72efacd540b321b99c9af10b68
Hrishike <hrishikesh.deodhar@gmail.com>
2017-03-06 15:14:56 +0530

    Update findAndDisableAgent.sh

--- findAndDisableAgent.sh
+++ findAndDisableAgent.sh
@@ -9,4 +9,4 @@ echo $agentUrl
 agentDetails=`curl -k $agentUrl -H 'Accept: application/vnd.go.cd.v4+json' | jq '{ "hostname": .hostname, "agent_config_state" : "Disabled" , "resources": ["Disabled'$resource_input'"], "environments" : [ ] }'`
 echo $agentDetails > agentdetails.json
 
-curl -k 'https://ec2-52-41-212-49.us-west-2.compute.amazonaws.com:8154/go/api/agents/e6a0ecfe-9536-498c-ba1f-7d9af7edaf3d' -H 'Accept: application/vnd.go.cd.v4+json' -H 'Content-Type: application/json' -X PATCH --data @agentdetails.json
+curl -k $agentUrl -H 'Accept: application/vnd.go.cd.v4+json' -H 'Content-Type: application/json' -X PATCH --data @agentdetails.json
