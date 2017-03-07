fd9e5457a8587424b84baddb910727ba724fe7d0
Ubuntu <ubuntu@ip-172-31-44-158.us-west-2.compute.internal>
2017-03-07 11:16:58 +0000

    Updated to handle multiple apps

--- perform-Blue-Green-Deployment.sh
+++ perform-Blue-Green-Deployment.sh
@@ -5,14 +5,10 @@ app_port=$2
 
 app_name=`echo $resource_input | tr '[:lower:]' '[:upper:]'`
 
-existingApp=`curl http://ec2-54-70-136-53.us-west-2.compute.amazonaws.com/eureka/apps -H "Content-Type:application/json" -H "Accept:application/json" | jq '.applications.application[] | select(.name == '\"$app_name\"') | "http://ec2-54-70-136-53.us-west-2.compute.amazonaws.com/eureka/apps/"+.name+"/"+.instance[].instanceId+"/status?value=OUT_OF_SERVICE"' | tr -d '\"'`
+existingApp=`curl http://ec2-54-70-136-53.us-west-2.compute.amazonaws.com/eureka/apps -H "Content-Type:application/json" -H "Accept:application/json" | jq '.applications.application[] | .instance[] | select(.app == '\"$app_name\"' and .status == "UP") | "http://ec2-54-70-136-53.us-west-2.compute.amazonaws.com/eureka/apps/"+.app+"/"+.instanceId+"/status?value=OUT_OF_SERVICE"' | tr -d '\"'`
+echo "Existing App URL = $existingApp"
 
-
-
- existAppInstanceId=`curl http://ec2-54-70-136-53.us-west-2.compute.amazonaws.com/eureka/apps/$app_name -H "Content-Type:application/json" -H "Accept:application/json" | jq '.application.instance[].instanceId' | tr -d '\"'`
-
-
-
+existAppInstanceId=`curl http://ec2-54-70-136-53.us-west-2.compute.amazonaws.com/eureka/apps/$app_name -H "Content-Type:application/json" -H "Accept:application/json" | jq '.application.instance[].instanceId' | tr -d '\"'`
 
 CONTAINER=$resource_input
 
@@ -40,28 +36,50 @@ echo `docker run --name $CONTAINER -d -P -e 'SPRING_PROFILES_ACTIVE=aws' -p $app
 
 sleep 30
 
-end=$((SECONDS+140))
+endFirst=$((SECONDS+140))
 
 echo "Please Wait!..... Pinging green $app_name app url to check status is UP or not."
 
-while [ $SECONDS -lt $end ]; do
+while [ $SECONDS -lt $endFirst ]; do
 
 newAppStatus=`curl -s http://ec2-54-70-136-53.us-west-2.compute.amazonaws.com/eureka/apps/$app_name -H "Content-Type:application/json" -H "Accept:application/json" | jq '.application.instance[] | select(.instanceId != '\"$existAppInstanceId\"') | .status' | tr -d '\"'`
 
 
+newAppInstanceId=`curl -s http://ec2-54-70-136-53.us-west-2.compute.amazonaws.com/eureka/apps/$app_name -H "Content-Type:application/json" -H "Accept:application/json" | jq '.application.instance[] | select(.instanceId != '\"$existAppInstanceId\"') | .instanceId' | tr -d '\"'`
 
-if [ "$newAppStatus" == "UP" ]; then
+
+
+if [ "$newAppStatus" == "OUT_OF_SERVICE" ]; then
     
-    echo "Bringing down blue $app_name app url"
-    curl -X PUT $existingApp
-    if [ $? -eq 0 ]; then
-     sleep 30
-     echo "Blue $app_name app url is OUT_OF_SERVICE"
-     exit 0
-    else
-     echo "Error: Unable to make Blue $app_name app url OUT_OF_SERVICE"
-     exit 1
-    fi
+    echo "Bringing UP green $app_name app url"
+    
+    curl -X PUT http://ec2-54-70-136-53.us-west-2.compute.amazonaws.com/eureka/apps/$app_name/$newAppInstanceId/status?value=UP
+    sleep 1
+    endSecond=$((SECONDS+60))
+    
+    echo "Please wait .... Checking whether green $app_name app is UP or not..."
+
+    while [ $SECONDS -lt $endSecond ]; do
+     
+    confirmNewAppStatus=`curl -s http://ec2-54-70-136-53.us-west-2.compute.amazonaws.com/eureka/apps/$app_name -H "Content-Type:application/json" -H "Accept:application/json" | jq '.application.instance[] | select(.instanceId != '\"$newAppInstanceId\"') | .status' | tr -d '\"'` 
+    
+	if [ "$confirmNewAppStatus" == "UP" ]; then
+	
+		echo "Successfully verified that green $app_name app is UP"
+                
+                echo "Please wait... Bringing down blue $app_name app url"
+    		curl -X PUT $existingApp
+    			if [ $? -eq 0 ]; then
+     			
+     				echo "Blue $app_name app url is OUT_OF_SERVICE"
+     				exit 0
+    			else
+     				echo "Error: Unable to make Blue $app_name app url OUT_OF_SERVICE"
+     				exit 1
+    			fi
+	 	
+        fi
+    done
 fi
 done
 
