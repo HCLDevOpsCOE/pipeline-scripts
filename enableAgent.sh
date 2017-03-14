77591649116186031863b112c51fb269621382d5
Hrishike <hrishikesh.deodhar@gmail.com>
2017-03-14 17:04:28 +0530

    Update enableAgent.sh

--- enableAgent.sh
+++ enableAgent.sh
@@ -2,7 +2,7 @@
 
 uuid=$1
 resource_input=$2
-agentDetails=`curl -k 'https://ec2-52-41-212-49.us-west-2.compute.amazonaws.com:8154/go/api/agents/'$uuid -H 'Accept: application/vnd.go.cd.v4+json'| jq '{ "hostname": .hostname, "agent_config_state" : "Enabled" , "resources": ['\"$resource_input\"'], "environments" : ["Deploy"] }'`
+agentDetails=`curl -k 'https://ec2-35-166-116-91.us-west-2.compute.amazonaws.com:8154/go/api/agents/'$uuid -H 'Accept: application/vnd.go.cd.v4+json'| jq '{ "hostname": .hostname, "agent_config_state" : "Enabled" , "resources": ['\"$resource_input\"'], "environments" : ["Deploy"] }'`
 echo $agentDetails > agentdetailstoUpdate.json
 
-curl -k 'https://ec2-52-41-212-49.us-west-2.compute.amazonaws.com:8154/go/api/agents/'$uuid -H 'Accept: application/vnd.go.cd.v4+json' -H 'Content-Type: application/json' -X PATCH --data @agentdetailstoUpdate.json
+curl -k 'https://ec2-35-166-116-91.us-west-2.compute.amazonaws.com:8154/go/api/agents/'$uuid -H 'Accept: application/vnd.go.cd.v4+json' -H 'Content-Type: application/json' -X PATCH --data @agentdetailstoUpdate.json
