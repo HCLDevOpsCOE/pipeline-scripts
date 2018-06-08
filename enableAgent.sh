94c1c5e0983f5ea5c3976bf09c45fc75b8052ecb
Ranjith <new.ranjith@gmail.com>
2018-06-08 19:42:24 +0530

    update enableAgent.sh
    updated url

--- enableAgent.sh
+++ enableAgent.sh
@@ -5,4 +5,4 @@ resource_input=$2
 agentDetails=`curl -k 'https://35.162.119.3:8154/go/api/agents/'$uuid -u 'admin:admin' -H 'Accept: application/vnd.go.cd.v4+json'| jq '{ "hostname": .hostname, "agent_config_state" : "Enabled" , "resources": ['\"$resource_input\"'], "environments" : ["Deploy"] }'`
 echo $agentDetails > agentdetailstoUpdate.json
 
-curl -k 'https://35.162.119.3:8154/go/api/agents/'$uuid -H 'Accept: application/vnd.go.cd.v4+json' -H 'Content-Type: application/json' -X PATCH --data @agentdetailstoUpdate.json
+curl -k 'https://35.162.119.3:8154/go/api/agents/'$uuid -u admin:admin' -H 'Accept: application/vnd.go.cd.v4+json' -H 'Content-Type: application/json' -X PATCH --data @agentdetailstoUpdate.json
