9d5996d480c89fe7eefe5508a586e16e0a5d3cc5
Ranjith <new.ranjith@gmail.com>
2018-06-08 12:21:18 +0530

    Update findAndDisableAgent.sh
    updated go server url

--- findAndDisableAgent.sh
+++ findAndDisableAgent.sh
@@ -3,7 +3,7 @@
 resource_input=$1
 echo $resource_input
 
-agentUrl=`curl -k 'https://ec2-54-70-136-53.us-west-2.compute.amazonaws.com:8154/go/api/agents' -H 'Accept: application/vnd.go.cd.v4+json' | jq '._embedded.agents[] | select(.resources[] =='\"$resource_input\"') | (._links.self.href)'| tr -d '"'`
+agentUrl=`curl -k 'https://35.162.119.3:8154/go/api/agents' -H 'Accept: application/vnd.go.cd.v4+json' | jq '._embedded.agents[] | select(.resources[] =='\"$resource_input\"') | (._links.self.href)'| tr -d '"'`
 echo $agentUrl
 
 agentDetails=`curl -k $agentUrl -H 'Accept: application/vnd.go.cd.v4+json' | jq '{ "hostname": .hostname, "agent_config_state" : "Disabled" , "resources": ["Disabled'$resource_input'"], "environments" : [ ] }'`
